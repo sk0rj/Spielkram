0707f8d6585acbd1dae7ce620d31f6244d851f24
sk0rj <skorj@outlook.de>
2024-03-03 17:30:01 +0100

    Update: Optimierung und Funktions-Erweiterung
    Update:
    - Zusammenführung von functions.py und gui.py
    
    Neu:
    - Integration von Zufallsantworten
    - Implementierung von Mechanismen zur Vermeidung von Falscheingaben
    - Verbesserte Fehlerbehandlung

--- Wa(h)l der Qual/main.py
+++ Wa(h)l der Qual/main.py
@@ -1,22 +1,10 @@
 from tkinter import Tk
-from gui import create_gui
-from functions import resource_path
+from functions import create_gui
 
 # Starten der Anwendung
 def torment_the_whale():
 
     root = Tk()
-
-    window_width = 300
-    window_height = 418
-
-    monitor_center_x = root.winfo_screenwidth() / 2 - (window_width / 2)
-    monitor_center_y = root.winfo_screenheight() / 2 - (window_height / 2)
-
-    root.geometry("%dx%d+%d+%d" % (window_width, window_height, monitor_center_x, monitor_center_y))
-    root.resizable(width=False, height=False)
-    root.title("Wa(h)l der Qual")
-    root.iconbitmap(resource_path("media/whale_icon.ico"))
 
     create_gui(root)
 
@@ -25,4 +13,4 @@ def torment_the_whale():
 if __name__ == "__main__":
     torment_the_whale()
 
-# pyinstaller --onefile --add-data "media/whale.png:media" --add-data "media/whale_icon.ico:media" --windowed --icon=media/whale_icon.ico main.py
+# pyinstaller --onefile --add-data "media\whale.png;media" --add-data "media\whale_icon.ico;media" --add-data "responses.json;." --windowed --icon=media\whale_icon.ico main.py
